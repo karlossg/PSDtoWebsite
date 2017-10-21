bae3cd023221a50525a10b24f948a5a69eb8da38
karlossg <karol.gratus@gmail.com>
2017-10-21 22:50:49 +0200

    grunt + more on gitignore

--- gruntfile.js
+++ gruntfile.js
@@ -8,15 +8,48 @@ module.exports = function(grunt) {
         },
         dist: {
           files: {
-            'css/main.css': 'sass/main.sass'
+            'css/style.css': 'css/style.scss'
           }
         }
-      }
+      },
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: './',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: './'
+            }]
+        }
+    },
+    browserSync: {
+        bsFiles: {
+            src : 'css/*.css'
+        },
+        options: {
+            watchTask: true,
+            server: {
+                baseDir: "./"
+            }
+        }
+    },
+    watch: {
+        scripts: {
+            files: ['css/*.scss'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            },
+        }
+    }
 
   });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
-
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
   // Default task(s).
-  grunt.registerTask('default', [sass]);
+  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
 };
